495e6ef041d30235c6b6203b88ea3b27d37c08bd
kis11 <59667818+kis11@users.noreply.github.com>
2020-05-02 01:43:34 -0500

    added email capability and attachment

--- app/screener2.py
+++ app/screener2.py
@@ -1,8 +1,44 @@
 import os
 import json
 import requests
+import datetime
 import pandas as pd
 import csv
+import base64
+from dotenv import load_dotenv
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import (
+    Mail, Attachment, FileContent, FileName,
+    FileType, Disposition, ContentId)
+
+
+def send_email():
+    load_dotenv()
+    date = datetime.date.today()
+    SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY", "OOPS, please set env var called 'SENDGRID_API_KEY'")
+    MY_EMAIL_ADDRESS = os.environ.get("MY_EMAIL_ADDRESS", "OOPS, please set env var called 'MY_EMAIL_ADDRESS'")
+    SENDGRID_TEMPLATE_ID = os.environ.get("SENDGRID_TEMPLATE_ID", "OOPS, please set env var called 'SENDGRID_TEMPLATE_ID'")
+    file_path = os.path.join(os.path.dirname(__file__), '..', "data", "screened_stocks.csv")
+    with open(file_path, 'rb') as f:
+        data = f.read()
+        f.close()
+    encoded = base64.b64encode(data).decode()
+    attachment = Attachment()
+    attachment.file_content = FileContent(encoded)
+    attachment.file_type = FileType('text/csv')
+    attachment.file_name = FileName('screened_stocks.csv')
+    attachment.disposition = Disposition('attachment')
+    template_data = {"human_friendly_timestamp": date.strftime("%B %d, %Y")}
+    client = SendGridAPIClient(SENDGRID_API_KEY) 
+    message = Mail(from_email=MY_EMAIL_ADDRESS, to_emails=MY_EMAIL_ADDRESS)
+    message.template_id = SENDGRID_TEMPLATE_ID 
+    message.dynamic_template_data = template_data
+    message.attachment = attachment
+    try:
+        client.send(message)
+    except Exception as e:
+        print("Oops, Sendgrid is down. Our bad.", e)
+
 
 csv_filepath = os.path.join(os.path.dirname(__file__), '..', "data", "stocklist.csv")
 allstock = pd.read_csv(csv_filepath)
@@ -13,7 +49,7 @@ profile = input("Are you a retiree, young investor, or an adult?")
 try:
     counter = 0
     listofstocks = pd.DataFrame()
-    while counter < 2500:
+    while counter < 11:
         symbol = allstock['Ticker Symbol'][counter]
         request_url = f"https://financialmodelingprep.com/api/v3/quote/{symbol}"
         response = requests.get(request_url)
@@ -28,17 +64,34 @@ except Exception:
 
 
 if profile == "young":
-    listofstocks = listofstocks[(listofstocks['pe'] > 20)]
-    listofstocks = listofstocks[['symbol', 'price', 'change', 'eps', 'pe' ]]
-    print(listofstocks)
+    listofstocks2 = listofstocks[(listofstocks['pe'] > 20)]
+    listofstocks3 = listofstocks2[['symbol', 'price', 'change', 'eps', 'pe' ]]
+    print(listofstocks3)
 
 if profile == "adult":
-    listofstocks == listofstocks[(listofstocks['pe']< 20)]
-    listofstocks = listofstocks[['symbol', 'price', 'change', 'eps', 'pe' ]]
-    print(listofstocks)
+    listofstocks2 = listofstocks[(listofstocks['pe']< 20)]
+    listofstocks3 = listofstocks2[['symbol', 'price', 'change', 'eps', 'pe' ]]
+    print(listofstocks3)
 
 if profile == "retiree":
-    listofstocks == listofstocks[(listofstocks['pe']< 12)]
-    listofstocks = listofstocks[['symbol', 'price', 'change', 'eps', 'pe' ]]
-    print(listofstocks)
+    listofstocks2 = listofstocks[(listofstocks['pe']< 12)]
+    listofstocks3 = listofstocks2[['symbol', 'price', 'change', 'eps', 'pe' ]]
+    print(listofstocks3)
+
+spread = input("Do you want to export this output to a spreadsheet sent to your email? If you do, enter yes.")
+
+while True:
+    if spread == "yes":
+        print("Ok, processing now. Thanks!")
+        send_email()
+        break
+    elif spread == "no":
+        print("Ok, sounds good. Thanks for using our service.")
+        break
+    else:
+        print("Sorry, didn't get that, please reply either yes or no.")
+
+
+
+
 
